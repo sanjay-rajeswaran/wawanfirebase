ceda632e1fd5fa0a3fbc550d2b3a4bc0e9667a77
nerdy21 <sanjayortiz@gmail.com>
2016-04-16 00:10:12 +0530

    heroku

--- wawanfirebase/settings.py
+++ wawanfirebase/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'fb',
 ]
 
@@ -118,5 +117,3 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
-STATIC_URL = '/static/'
